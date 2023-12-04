73033860a771497ed9529311173f3710b1827315
Diego Braga <diego.braga@gmail.com>
2023-12-04 01:12:34 -0300

    feat add layout in view user

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-Br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Series</title>
+    <script src="https://kit.fontawesome.com/331839995d.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container">
+    <div class="jumbotron">
+        <h1>@yield('cabecalho')</h1>
+    </div>
+</div>
+@yield('conteudo')
+</body>
+</html>
--- resources/views/user/form.blade.php
+++ resources/views/user/form.blade.php
@@ -1,35 +1,64 @@
 <!-- resources/views/cadastro.blade.php -->
 
+
+@extends('layout')
+
+@section('cabecalho')
+Cadastro de Usuário
+@endsection
+
+@section('conteudo')
+
+<div class="container">
 <form method="post" action="/user/register">
     @csrf
 
+    @error('name')
+    <div class="alert alert-dark">
+        {{ $message }}
+     </div>
+    @enderror
+    @error('name')
+    <div class="alert alert-dark">
+        {{ $message }}
+     </div>
+    @enderror
+    @error('email')
+    <div class="alert alert-dark">
+        {{ $message }}
+     </div>
+    @enderror
+    @error('password')
+    <div class="alert alert-dark">
+        {{ $message }}
+     </div>
+    @enderror
+    @error('password_confirmation')
+    <div class="alert alert-dark">
+        {{ $message }}
+     </div>
+    @enderror
+    
+
     <label for="name">Nome:</label>
     <input type="text" name="name" value="{{ old('name') }}">
-    @error('name')
-        <div>{{ $message }}</div>
-    @enderror
+   
 
     <label for="email">E-mail:</label>
     <input type="text" name="email" value="{{ old('email') }}">
-    @error('email')
-        <div>{{ $message }}</div>
-    @enderror
+  
 
     <label for="password">Senha:</label>
     <input type="password" name="password">
-    @error('password')
-        <div>{{ $message }}</div>
-    @enderror
+   
 
     <label for="password_confirmation">Confirmação de Senha:</label>
     <input type="password" name="password_confirmation">
-    @error('password_confirmation')
-        <div>{{ $message }}</div>
-    @enderror
+   
 
     <button type="submit">Cadastrar</button>
 </form>
-
+</div>
 <script>
     var successMessage = "{{ session('success') }}";
     
@@ -37,3 +66,7 @@
         alert(successMessage);
     }
 </script>
+
+@endsection
+
+
